8a240c2dddbce732b13587e7fadb5ea22f486a99
lilkarti <69488258+lilkarti@users.noreply.github.com>
2020-08-24 19:05:04 -0400

    Find area of rectangle

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	float area, lenght, width; 
+	cout<<"Enter lenght and width of rectangle: ";
+	cin>>lenght>>width;
+	area= lenght*width; 
+	cout<<"The area of the rectangle is: "<< area;
+}
