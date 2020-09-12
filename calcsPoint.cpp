520c316208f706ca2bee55c282615d65e3956754
lilkarti <69488258+lilkarti@users.noreply.github.com>
2020-09-12 18:52:29 -0400

    Sum, Average, Functions
    Sum, Average, Functions using pointers and functions

---
+++ calcsPoint.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void sumAvgProd(float num1, float num2, float *sum, float *avg, float *prod)
+{
+	*sum= num1+num2; 
+	*avg= *sum/(2);
+	*prod= (num1*num2);
+}
+void input(float *num1, float *num2)
+{
+	cout<<"Enter two float values: ";
+	cin>>*num1>>*num2;
+}
+void output(float num1, float num2, float sum, float avg, float prod)
+{
+	cout<<"\nSum: "<<sum;
+	cout<<"\nAverage: "<<avg;
+	cout<<"\nProduct: "<<prod; 
+}
+
+int main() 
+{
+	float x,y,sum,avg,prod;
+	input(&x,&y);
+	sumAvgProd(x,y,&sum,&avg,&prod);
+	output(x,y,sum,avg,prod);
+}
+
+
+
